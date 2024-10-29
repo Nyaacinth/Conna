68a5e6e7a1ac00c44c665263263974ee234471a8
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-10-29 21:38:16 +0800

    use LightningCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import unocss from "@unocss/svelte-scoped/vite"
 import transformerDirectives from "@unocss/transformer-directives"
+import { browserslistToTargets } from "lightningcss"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+    css: {
+        transformer: "lightningcss",
+        lightningcss: {
+            targets: browserslistToTargets(["edge 88", "firefox 78", "chrome 87", "safari 14"])
+        }
+    },
+    build: {
+        target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+        cssMinify: "lightningcss"
+    },
     plugins: [
         unocss({
             injectReset: "@unocss/reset/tailwind-compat.css",
