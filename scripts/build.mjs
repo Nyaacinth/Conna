727b961f4236e10cded0e448d7690781368a81ba
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-01-07 19:17:36 +0800

    Add Build Support

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,27 +1,27 @@
-import os from "node:os";
-import {spawn} from "node:child_process";
-import process from "node:process";
+import os from "node:os"
+import { spawn } from "node:child_process"
+import process from "node:process"
+import { dirname, sep as separator } from "node:path"
+import { fileURLToPath } from "node:url"
 
 const scapp = {
     win32: "./bin/windows/x64/scapp.exe",
     linux: "./bin/linux/x64/scapp",
-    darwin: "./bin/macosx/scapp",
-};
+    darwin: "./bin/macosx/scapp"
+}
 
 try {
-    const arguments_ = [
-        "build.htm",
-        "--debug",
-    ];
+    const dir = dirname(fileURLToPath(import.meta.url)) + separator
+    const arguments_ = [dir + "build.htm", "--debug"]
 
-    const platform = os.platform();
+    const platform = os.platform()
 
     spawn(scapp[platform], arguments_, {
-        detached: true,
-    });
+        detached: true
+    })
 } catch (error) {
-    console.error(`\u001B[31m${error}\u001B[0m`);
+    console.error(`\u001B[31m${error}\u001B[0m`)
 }
 
 // do not wait for child processes
-process.exit(0);
+process.exit(0)
