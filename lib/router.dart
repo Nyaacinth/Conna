08cdd637f48c4cbf8c1bc6002d4660127ce905c3
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-09-11 00:20:57 +0800

    Add router

---
+++ lib/router.dart
@@ -0,0 +1,11 @@
+import 'package:conna/routes/index.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => Index(),
+    ),
+  ],
+);
